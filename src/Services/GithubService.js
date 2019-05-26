8ff7b8750dd59845f3bccce999a9fda0608a7a9d
dydeepak97 <deepak.yadav@getpostman.com>
2019-05-27 02:55:20 +0530

    Added url-parser in github service

--- src/Services/GithubService.js
+++ src/Services/GithubService.js
@@ -1,19 +1,21 @@
 import { makeApiCall } from "./UtilService";
+import url from 'url';
 
 const GITHUB_API_HOST = 'api.github.com',
   issueEx = "http://api.github.com/repos/octocat/Hello-World/issues{/number}";
 
 
-export function getIssuesForRepository(repoPath, cb) {
-  let requestOptions = {
-    protocol: 'https',
-    hostname: GITHUB_API_HOST,
-    pathname: `repos/${repoPath}/issues`,
-    query: {
-      state: 'open',
-      per_page: 100
+export function getIssuesForRepository(repoUrl, cb) {
+  let repoPath = url.parse(repoUrl).pathname,
+    requestOptions = {
+      protocol: 'https',
+      hostname: GITHUB_API_HOST,
+      pathname: `repos/${repoPath}/issues`,
+      query: {
+        state: 'open',
+        per_page: 100
+      }
     }
-  }
 
   return makeApiCall(requestOptions, cb);
 };
